d2a4e517579493fb4b58c7aaab559df978e420b0
Jeremah <seunjeremiah@gmail.com>
2025-04-06 00:15:25 +0100

    Files arrangement

--- lib/features/home/components/product_card.dart
+++ lib/features/home/components/product_card.dart
@@ -1,5 +1,5 @@
 import 'package:credpal_test/constants/app_colors.dart';
-import 'package:credpal_test/features/components/cd_text.dart';
+import 'package:credpal_test/features/home/components/cd_text.dart';
 import 'package:credpal_test/models/product.dart';
 import 'package:flutter/material.dart';
 
