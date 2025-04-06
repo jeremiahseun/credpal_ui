fe5888cb9179d74c8d6f4c31fdfd15c3c785720f
Jeremah <seunjeremiah@gmail.com>
2025-04-06 19:47:51 +0100

    Screenutil and Basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'features/home/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  // Ensure Flutter is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set preferred orientations to portrait only
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(414, 896),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp(
+            title: 'CredPal',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme:
+                  ColorScheme.fromSeed(seedColor: const Color(0xFF2D5BFF)),
+              useMaterial3: true,
+              scaffoldBackgroundColor: Colors.white,
+            ),
+            home: const HomeScreen(),
+          );
+        });
   }
 }
